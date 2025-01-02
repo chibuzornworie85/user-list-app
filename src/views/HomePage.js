a5fc299cfa75ae4f207ec71b187f2746f7702399
GewinCodeX <chibuzornworie85@gmail.com>
2025-01-02 18:37:11 +0100

    feat: implement user list display with links to detailed user profile

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -26,7 +26,7 @@ function HomePage() {
             </div>
             <Link
               to={`/user/${user.id}`}
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 whitespace-nowrap"
             >
               View Details
             </Link>
